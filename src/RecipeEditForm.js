bc9ab2a9497fbdd5ff948a03fb6bf95c878b821c
coucou_miya <ybb.emily@gmail.com>
2018-11-05 17:57:31 +0100

    fetch data

--- src/RecipeEditForm.js
+++ src/RecipeEditForm.js
@@ -1,6 +1,40 @@
 import React from "react";
-const RecipeEditForm = ({ match }) => {
-  return <h2>RecipeEditForm: {match.params.id}</h2>;
-};
+import config from "./config";
+
+class RecipeEditForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: {},
+      id: props.match.params.id
+    };
+  }
+  componentDidMount() {
+    fetch(`${config.API_URL}/recipes/${this.state.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw res.text();
+        }
+      })
+      .then(response => {
+        console.log(response);
+        this.setState({ recipe: response });
+      })
+      .catch(err => console.log(err));
+  }
+  // TODO: render recipe detail
+  // TODO: submit edidtion
+  // TODO: delete recipe
+  render() {
+    return <h2>RecipeEditForm: {this.props.match.params.id}</h2>;
+  }
+}
 
 export default RecipeEditForm;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,6 +1,35 @@
 import React from "react";
+import config from "./config";
 
-const RecipeList = () => {
-  return <h2>RecipeList</h2>;
-};
+class RecipeList extends React.Component {
+  state = {
+    recipes: []
+  };
+  componentDidMount() {
+    fetch(config.API_URL + "/recipes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw res.text();
+        }
+      })
+      .then(response => {
+        console.log(response);
+        // this.setState({ recipes: res });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    // TODO: render all recipes: basic info
+    // TODO: active, remove, edit recipe
+    return <h2>RecipeList</h2>;
+  }
+}
+
 export default RecipeList;
